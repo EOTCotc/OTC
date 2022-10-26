81b06a67a95ed6a52cbc8066c40c2f33bfd51603
2292777095 <2292777095@qq.com>
2022-10-26 11:32:41 +0800

    修改了请求地址

--- TRX/src/api/http.js
+++ TRX/src/api/http.js
@@ -1,15 +1,15 @@
-import axios from "axios"
+import axios from 'axios';
 
 const http = axios.create({
-  baseURL: 'http://192.168.10.107:5555/',
-  timeout: 10000
-})
+  baseURL: process.env.VUE_APP_BASE_URL,
+  timeout: 10000,
+});
 
 const wallet = {
-  walletAddress: localStorage.getItem("myaddress"),
-  otype: localStorage.getItem("netType"),
-  sign: localStorage.getItem("mysign"),
-}
+  walletAddress: localStorage.getItem('myaddress'),
+  otype: localStorage.getItem('netType'),
+  sign: localStorage.getItem('mysign'),
+};
 // const wallet = {
 //   walletAddress: 'TQr23x4mPP1oH4soAT7w23jWMbXt6x6jVU',
 //   otype: 'xxx',
@@ -17,19 +17,19 @@ const wallet = {
 // }
 //请求拦截器
 http.interceptors.request.use(
-  (config) => {
-    config.data = Object.assign(config.data || {}, wallet)
+  config => {
+    config.data = Object.assign(config.data || {}, wallet);
     return config;
   },
-  (error) => Promise.reject(error)
+  error => Promise.reject(error)
 );
 
 //响应拦截器
 http.interceptors.response.use(
   function (res) {
-    const {code} = res.data
+    const { code } = res.data;
     if (code) {
-      return Promise.reject(res.data)
+      return Promise.reject(res.data);
     }
     return res.data;
   },
